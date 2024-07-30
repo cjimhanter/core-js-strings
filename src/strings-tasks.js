7b48b346fee573bdffec6a72ce06c70a4a36de94
Denis <cjim57256@gmail.com>
2024-07-30 20:21:12 +0200

    feat: do some tasks -(Tue, Jul 30, 2024 08:21:12 PM)-

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -267,8 +267,8 @@ function reverseString(str) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  return str.split('').sort().join('');
 }
 
 /**
@@ -283,8 +283,8 @@ function orderAlphabetically(/* str */) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
@@ -301,8 +301,9 @@ function containsSubstring(/* str, substring */) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vovels = str.match(/[aeiouy]/gi);
+  return vovels === null ? 0 : vovels.length;
 }
 
 /**
